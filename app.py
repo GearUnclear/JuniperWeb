12396e5a6ff17c389ad312b3f15b6d56b8fbbd97
Danefrak <dane@gearunclear.com>
2023-10-31 16:54:34 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   app.py 	new file:   templates/index.html 	deleted:    templates/search.html

--- app.py
+++ app.py
@@ -1,48 +1,64 @@
-# Modified app.py based on the pseudocode for troubleshooting and fixing the application
-
-# Step 1: Check Imports and Dependencies
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import pandas as pd
+import os
 
-# Step 2: Validate Data File
-# Assuming pstep.csv is in the same directory as app.py for this example. If not, provide the full path.
-data_path = "pstep.csv"
-try:
-    df = pd.read_csv(data_path)
-except FileNotFoundError:
-    df = pd.DataFrame()  # Empty DataFrame if file not found
+# pip install flask pandas
 
-# Step 3: Examine Routes
 app = Flask(__name__)
 
-@app.route('/')
+# Load data
+data_path = r"Y:\pyrq backup\pyrq - Copy - Copy\pstep.csv"
+df = pd.read_csv(data_path)
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('search.html')
+    return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
-def search_unit():
-    # Step 4: Form Data Handling
-    unit_number = request.form.get('unit_number', '').replace(" ", "").lower()
-    
-    if not unit_number:
-        return jsonify({"error": "Unit number is missing"}), 400
-    
-    # Step 5: Data Query
-    if df.empty:
-        return jsonify({"error": "Data file missing or empty"}), 500
-    
-    try:
-        # Assuming columns 'Building Number' and 'Unit Number' exist in the DataFrame
-        query_string = df['Building Number'].astype(str).str.lower() + df['Unit Number'].astype(str).str.lower()
-        result = df[query_string == unit_number]
-        
-        if result.empty:
-            return jsonify({"error": "Unit not found"}), 404
-        
-        return jsonify(result.to_dict(orient='records'))
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+def search():
+    unit_number = request.form.get('unitNumber').replace(" ", "").lower()
+    all_results = search_unit(unit_number)
+    details = [show_details(row) for row in all_results]
+    return render_template('index.html', results=all_results, details=details)
 
-# Running the application
+def search_unit(unit_number):
+    result1 = df[df['Building Number'].astype(str).str.lower().str.cat(df['Unit Number'].astype(str).str.lower(), sep="") == unit_number]
+    result2 = df[df['Unit Number'].astype(str).str.lower() == unit_number]
+    result3 = df[df['Unit Number'].astype(str).str.lower().str.cat(df['Building Number'].astype(str).str.lower(), sep="") == unit_number]
+    result4 = df[df['Building Number'].astype(str).str.lower() == unit_number]
+
+    all_results = pd.concat([result1, result2, result3, result4]).drop_duplicates().to_dict('records')
+    return all_results
+
+def show_details(unit_row):
+    details_list = []
+    fields = [
+        ('Address Line 1', 'Address: line1'),
+        ('Address Line 2', 'Address: line2'),
+        ('Site', 'Site'),
+        ('Program Name in HMIS', 'Program Name in HMIS'),
+        ('Building Number', 'Building Number'),
+        ('Unit Number', 'Unit Number'),
+        ('Status', 'Status'),
+        ('Family Size', 'Family Size'),
+        ('Household Type', 'Household Type'),
+        ('Staff Assigned', 'Staff Assigned'),
+        ('Unit Type', 'Unit Type'),
+        ('Bed Inventory', 'Bed Inventory'),
+        ('Disability Required', 'Disability Required'),
+        ('Chronic Homeless', 'Chronic Homeless'),
+        ('Age Range', 'Age Range'),
+        ('AMI Limit', 'AMI Limit'),
+        ('Additional Details', 'Additional Details'),
+        ('Unit Notes', 'Unit Notes'),
+        ('Assigned Programs', 'Assigned Programs')
+    ]
+    
+    for label, column in fields:
+        if not pd.isna(unit_row.get(column, '')):
+            details_list.append(f"{label}: {unit_row[column]}")
+    
+    return "\n".join(details_list)
+
 if __name__ == '__main__':
     app.run(debug=True)
